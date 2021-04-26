0abbce51ba7d2d482fc9de4a2b9dff044a0bfed3
DavidRedrejo <82117809+DavidRedrejo@users.noreply.github.com>
2021-04-26 22:46:41 +0200

    Interfaz inicial
    Fase inicial de la interfaz gráfica: 
    - selector de método de dimensionado

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
-#include <iostream>
-#include"clases_lotes.h"
-using namespace std;
-int main()
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
 {
-    EOQ lote1("Cervezas",3000,200,2);
-         POQ lote2("Colchones",4000,250,1,75,100);
-         EOQruptura lote3("Botellas",3500,150,3,1);
-         POQruptura lote4("bolis",4000,250,1,75,100,1);
-         lote1.mostrar();
-         lote1.calculoEOQ();
-         lote2.mostrar();
-         lote2.calculoPOQ();
-         lote3.mostrar();
-         lote3.calculoEOQruptura();
-         lote4.mostrar();
-         lote4.calculoPOQruptura();
-         system("pause");
-         return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
