84fbc9511935f0c35dba3ac2a70eb23d80af918a
Manueldri99 <82500556+Manueldri99@users.noreply.github.com>
2021-04-28 18:12:44 +0200

    Cambios lotes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,9 +28,39 @@ bool MainWindow::recoger_datos(){
             return recoge_bien=false;
         qDebug("Recoge datos");
     }
-    else if(metodo=="POQ"){qDebug("pruebaPOQ");}
-    else if(metodo=="EOQr"){qDebug("pruebaEOQ r");}
-    else if(metodo=="POQr"){qDebug("pruebaPOQ r");}
+    else if(metodo=="POQ"){
+        producto = ui->line_producto_3->text().toStdString();
+        demanda_anual = ui->line_dem_anu_3->text().toFloat();
+        cEmision = ui->line_cost_emi_3->text().toFloat();
+        cPosesion = ui->line_cost_pos_3->text().toFloat();
+        demanda_diaria = ui->line_deman_diaria->text().toFloat();
+        prod_diaria = ui->line_prod_diaria->text().toFloat();
+        if (producto == "" || demanda_anual == 0.0 || cEmision == 0.0 || cPosesion == 0.0 || demanda_diaria == 0.0 || prod_diaria == 0.0)
+            return recoge_bien=false;
+        qDebug("Recoge datos");
+        qDebug("pruebaPOQ");}
+    else if(metodo=="EOQr"){
+        producto = ui->line_producto_4->text().toStdString();
+        demanda_anual = ui->line_dem_anu_4->text().toFloat();
+        cEmision = ui->line_cost_emi_4->text().toFloat();
+        cPosesion = ui->line_cost_pos_4->text().toFloat();
+        cRuptura = ui->line_cost_rupt->text().toFloat();
+        if (producto == "" || demanda_anual == 0.0 || cEmision == 0.0 || cPosesion == 0.0 || cRuptura == 0.0)
+            return recoge_bien=false;
+        qDebug("Recoge datos");
+        qDebug("pruebaEOQ r");}
+    else if(metodo=="POQr"){
+        producto = ui->line_producto_5->text().toStdString();
+        demanda_anual = ui->line_dem_anu_5->text().toFloat();
+        cEmision = ui->line_cost_emi_5->text().toFloat();
+        cPosesion = ui->line_cost_pos_5->text().toFloat();
+        demanda_diaria = ui->line_deman_diaria_2->text().toFloat();
+        prod_diaria = ui->line_prod_diaria_2->text().toFloat();
+        cRuptura = ui->line_cost_rupt_2->text().toFloat();
+        if (producto == "" || demanda_anual == 0.0 || cEmision == 0.0 || cPosesion == 0.0 || demanda_diaria == 0.0 || prod_diaria == 0.0 || cRuptura == 0.0)
+            return recoge_bien=false;
+        qDebug("Recoge datos");
+        qDebug("pruebaPOQ r");}
     return recoge_bien;
 }
 
@@ -48,11 +78,28 @@ void MainWindow::on_bt_calcular_tam_lote_clicked()
         ui->line_tam_lote->setText(QString::number(tamano_lote));
         qDebug("calcula eoq");
     }
-    else if(metodo=="POQ"){qDebug("pruebaPOQ");}
-    else if(metodo=="EOQr"){qDebug("pruebaEOQ r");}
-    else if(metodo=="POQr"){qDebug("pruebaPOQ r");}
-
-
+    else if(metodo=="POQ"){
+        POQ lote(producto, demanda_anual, cEmision, cPosesion, demanda_diaria, prod_diaria);
+        tamano_lote=lote.calculoPOQ();
+        lote.mostrar();
+        ui->line_tam_lote->setText(QString::number(tamano_lote));
+        qDebug("calcula poq");
+        qDebug("pruebaPOQ");
+    }
+    else if(metodo=="EOQr"){
+        EOQruptura lote(producto, demanda_anual, cEmision, cPosesion, cRuptura);
+        tamano_lote=lote.calculoEOQruptura();
+        lote.mostrar();
+        ui->line_tam_lote->setText(QString::number(tamano_lote));
+        qDebug("calcula eoqr");
+        qDebug("pruebaEOQ r");}
+    else if(metodo=="POQr"){
+        POQruptura lote(producto, demanda_anual, cEmision, cPosesion, demanda_diaria, prod_diaria, cRuptura);
+        tamano_lote=lote.calculoPOQruptura();
+        lote.mostrar();
+        ui->line_tam_lote->setText(QString::number(tamano_lote));
+        qDebug("calcula poqr");
+        qDebug("pruebaPOQ r");}
 }
 
 void MainWindow::on_tabWidget_currentChanged(int index)
